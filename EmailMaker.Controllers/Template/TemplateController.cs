da00bba0bdb5dd4ceeadc2d804b5985bda5726c2
xhafan <xhafan@58660d98-57f0-c742-a5bf-f388f74cbcd1>
2011-01-25 20:04:06 +0000

    mvc 3 website project added

---
+++ EmailMaker.Controllers/Template/TemplateController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace EmailMaker.Controllers.Template
+{
+    public class TemplateController : Controller
+    {
+        //
+        // GET: /Template/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
