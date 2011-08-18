55969a34d682b6b3a1d1574a8080ea4b1b65f5de
xhafan <xhafan@58660d98-57f0-c742-a5bf-f388f74cbcd1>
2011-08-18 00:13:19 +0000

    clean up

--- EmailMaker.Queries/Messages/GetEmailRecipientIdsQueryMessage.cs
+++
@@ -1,9 +0,0 @@
-using Core.Queries;
-
-namespace EmailMaker.Queries.Messages
-{
-    public class GetEmailRecipientIdsQueryMessage : IQueryMessage
-    {
-        public int EmailId { get; set; }
-    }
-}
