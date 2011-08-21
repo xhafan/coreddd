c11d7a1df8cef5e23f52f6a9c6a22cad2ae1c686
xhafan <xhafan@58660d98-57f0-c742-a5bf-f388f74cbcd1>
2011-08-21 12:41:25 +0000

    menu and home page added

---
+++ EmailMaker.Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace EmailMaker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
