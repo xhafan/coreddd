062cf353bd9dff1db2bd17dd7256c3c00ff785e3
xhafan <xhafan@58660d98-57f0-c742-a5bf-f388f74cbcd1>
2011-08-22 22:47:19 +0000

    BuildUrlFromExpression used from Microsoft.Web.Mvc.dll to generate link urls

--- Core/Core.Web/Extensions/HtmlExtensions.cs
+++ Core/Core.Web/Extensions/HtmlExtensions.cs
@@ -7,7 +7,7 @@ namespace Core.Web.Extensions
 {
     public static class HtmlExtensions
     {
-        public static string UrlFromControllerMethod<TController>(this HtmlHelper htmlHelper, Expression<Action<TController>> action) where TController : Controller
+        public static string BuildUrlFromExpressionWithoutParameters<TController>(this HtmlHelper htmlHelper, Expression<Action<TController>> action) where TController : Controller
         {
             var url = new UrlHelper(htmlHelper.ViewContext.RequestContext);
             var routeValues = RouteBuilder.GetRouteValuesFromExpression(action, false);
