92c2f32189b8588d11cc2f467bdf96f3adb5c713
xhafan <xhafan@58660d98-57f0-c742-a5bf-f388f74cbcd1>
2012-10-04 21:32:03 +0000

    CoreTest library upgraded to version 1.1

--- CoreTest/BaseTest.cs
+++ CoreTest/BaseTest.cs
@@ -1,4 +1,5 @@
-﻿using Rhino.Mocks;
+﻿using CoreDdd.Domain;
+using Rhino.Mocks;
 
 namespace CoreTest
 {
@@ -6,17 +7,30 @@ namespace CoreTest
     {
         protected T Stub<T>(params object[] argumentsForConstructor) where T : class
         {
-            return Mock<T>(argumentsForConstructor); // mock is here otherwise you cannot stub properties (bug in RhinoMocks 3.6)
+            var stub = Mock<T>(argumentsForConstructor);
+            StubEqualityForEntities(stub);
+            return stub; // mock is here otherwise you cannot stub properties (bug in RhinoMocks 3.6)
         }
 
         protected T Mock<T>(params object[] argumentsForConstructor) where T : class
         {
-            return MockRepository.GenerateMock<T>(argumentsForConstructor);
+            var mock = MockRepository.GenerateMock<T>(argumentsForConstructor);
+            StubEqualityForEntities(mock);
+            return mock;
         }
 
         protected T PartialMock<T>(params object[] argumentsForConstructor) where T : class
         {
             return MockRepository.GeneratePartialMock<T>(argumentsForConstructor);
+        }
+
+        protected void StubEqualityForEntities(object obj)
+        {
+            var type = obj.GetType();
+            if (type.IsSubclassOfRawGeneric(typeof(Entity<>)))
+            {
+                obj.Stub(x => x.Equals(obj)).Return(true);
+            }
         }
     }
 }
--- CoreTest/Properties/AssemblyInfo.cs
+++ CoreTest/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CoreTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("xhafan")]
 [assembly: AssemblyProduct("CoreTest")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("open source")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
