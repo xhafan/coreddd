e6c5f6d60c68ed1daf2e359d06b2c4d1f8a148c6
xhafan <xhafan@58660d98-57f0-c742-a5bf-f388f74cbcd1>
2012-10-04 22:40:52 +0000

    CoreTest version 1.1.1 chaining support for Stub / Return added

---
+++ CoreTest/RhinoMocksExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Rhino.Mocks.Interfaces;
+
+namespace Rhino.Mocks
+{
+    // Chaining supports for Stub / Return
+    public static class RhinoMocksExtensions
+    {
+        public static Tuple<T, IMethodOptions<TReturn>> Stubs<T, TReturn>(this T target, Function<T, TReturn> action)
+                   where T : class
+        {
+            return new Tuple<T, IMethodOptions<TReturn>>(target, target.Stub(action));
+        }
+
+        public static T Returns<T, TReturn>(this Tuple<T, IMethodOptions<TReturn>> targetAndMethodOptions, TReturn objToReturn)
+            where T : class
+        {
+            targetAndMethodOptions.Item2.Return(objToReturn);
+            return targetAndMethodOptions.Item1;
+        }
+    }
+}
