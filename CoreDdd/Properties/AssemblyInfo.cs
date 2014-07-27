1a86b8c1e920c88fd29904dc7a8d0bc03d4660c2
xhafan <xhafan@58660d98-57f0-c742-a5bf-f388f74cbcd1>
2014-07-27 12:53:06 +0000

    Core libs version set to 2.0

--- CoreDdd/Properties/AssemblyInfo.cs
+++ CoreDdd/Properties/AssemblyInfo.cs
@@ -1,37 +1,6 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("CoreDdd")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("xhafan")]
 [assembly: AssemblyProduct("CoreDdd")]
-[assembly: AssemblyCopyright("open source")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("CoreDdd.Tests")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6fb6080b-d4f6-4db2-9cda-7ff09f4a4ebf")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.7.1")]
-[assembly: AssemblyFileVersion("1.1.7.1")]
---
+++ CoreSharedAssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("xhafan")]
+[assembly: AssemblyCopyright("open source")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("2.0.*")]
