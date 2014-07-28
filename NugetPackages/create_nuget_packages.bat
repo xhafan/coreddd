1739f3bbf06af2ce5314fc5aec2b7632b89b61f0
xhafan <xhafan@58660d98-57f0-c742-a5bf-f388f74cbcd1>
2014-07-28 20:18:48 +0000

    Nuget packages moved to trunk\NugetPackages folder

---
+++ NugetPackages/create_nuget_packages.bat
@@ -0,0 +1,29 @@
+cd CoreDdd
+..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
+
+cd ..\CoreDdd.Nhibernate
+..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
+
+cd ..\CoreDdd.Nhibernate.Register.Castle
+..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
+
+cd ..\CoreDdd.Register.Castle
+..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
+
+cd ..\CoreIntegrationTest.Nhibernate
+..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
+
+cd ..\CoreIoC
+..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
+
+cd ..\CoreNserviceBusTest
+..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
+
+cd ..\CoreTest
+..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
+
+cd ..\CoreUtils
+..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
+
+cd ..\CoreWeb
+..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
