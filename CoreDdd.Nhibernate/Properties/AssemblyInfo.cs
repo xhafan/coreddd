5ae64b0b78498a9423990b3c21f4812018e50388
xhafan <xhafan@58660d98-57f0-c742-a5bf-f388f74cbcd1>
2014-08-17 21:39:04 +0000

    CoreDdd.Nhibernate version set to 2.1.2

--- CoreDdd.Nhibernate/Properties/AssemblyInfo.cs
+++ CoreDdd.Nhibernate/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CoreDdd.Nhibernate")]
 [assembly: AssemblyProduct("CoreDdd.Nhibernate")]
 [assembly: Guid("e14f9f38-d0cd-486b-8134-b217255ada8c")]
-[assembly: AssemblyVersion("2.1.1.*")]
+[assembly: AssemblyVersion("2.1.2.*")]
