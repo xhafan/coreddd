9beae8ab46f12ac6510099a867432dd3011bdf4a
Martin Havlista <xhafan@gmail.com>
2018-01-17 10:37:13 +0000

    Refactored RhinoMocksExtensions - replaced Tuple with ValueTuple

--- CoreTest/RhinoMocksExtensions.cs
+++ CoreTest/RhinoMocksExtensions.cs
@@ -1,22 +1,21 @@
-﻿using System;
-using Rhino.Mocks.Interfaces;
+﻿using Rhino.Mocks.Interfaces;
 
 namespace Rhino.Mocks
 {
     // Chaining supports for Stub / Return
     public static class RhinoMocksExtensions
     {
-        public static Tuple<T, IMethodOptions<TReturn>> Stubs<T, TReturn>(this T target, Function<T, TReturn> action)
+        public static (T Target, IMethodOptions<TReturn> MethodOptions) Stubs<T, TReturn>(this T target, Function<T, TReturn> action)
                    where T : class
         {
-            return new Tuple<T, IMethodOptions<TReturn>>(target, target.Stub(action));
+            return (target, MethodOptions: target.Stub(action));
         }
 
-        public static T Returns<T, TReturn>(this Tuple<T, IMethodOptions<TReturn>> targetAndMethodOptions, TReturn objToReturn)
+        public static T Returns<T, TReturn>(this (T Target, IMethodOptions<TReturn> MethodOptions) targetAndMethodOptions, TReturn objToReturn)
             where T : class
         {
-            targetAndMethodOptions.Item2.Return(objToReturn);
-            return targetAndMethodOptions.Item1;
+            targetAndMethodOptions.MethodOptions.Return(objToReturn);
+            return targetAndMethodOptions.Target;
         }
     }
 }
