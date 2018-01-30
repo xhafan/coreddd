75993d4834897bf087736cbc29c40a3ffa22c220
xhafan <xhafan@gmail.com>
2018-01-30 20:50:32 +0000

    changed target framework from .net 4.0 to .net 4.6.1, converted some projects to .net standard 2.0

--- EmailMaker.Controllers/ViewModels/AccountModels.cs
+++ EmailMaker.Controllers/ViewModels/AccountModels.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Web.Mvc;
 using System.Web.Security;
+using Compare=System.ComponentModel.DataAnnotations.CompareAttribute;
 
 namespace EmailMaker.Controllers.ViewModels
 {
