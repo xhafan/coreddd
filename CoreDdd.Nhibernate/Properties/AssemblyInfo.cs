f677f7dec223e3783f7d9a592cfdbfdb19fdffd3
xhafan <xhafan@gmail.com>
2018-01-31 18:05:31 +0000

    converted CoreDdd.Nhibernate.csproj file into a new format

--- CoreDdd.Nhibernate/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("CoreDdd.Nhibernate")]
-[assembly: AssemblyProduct("CoreDdd.Nhibernate")]
-[assembly: Guid("e14f9f38-d0cd-486b-8134-b217255ada8c")]
-[assembly: AssemblyVersion("2.1.3.*")]
