3d9e99203fd941d634138fcd75ede30ea8bac37b
Martin Havlista <xhafan@gmail.com>
2018-01-31 12:24:16 +0000

    changed target framework of CoreDdd to .netstandard 2.0, changed assembly versions to 3.0.0 for some core libs

--- CoreIoC.Castle/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("CoreIoC.Castle")]
-[assembly: AssemblyProduct("CoreIoC.Castle")]
-[assembly: Guid("ca0cbc36-7bd2-4f8c-9829-7966f0450aa6")]
-[assembly: AssemblyVersion("2.0.0.0")]
--- CoreUtils/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("CoreUtils")]
-[assembly: AssemblyProduct("CoreUtils")]
-[assembly: Guid("0d07b478-edea-4919-9c52-84a7942961c7")]
-[assembly: AssemblyVersion("2.0.0.0")]
