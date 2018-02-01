0ce849473c42d213e805333ea106f749e72a926c
xhafan <xhafan@gmail.com>
2018-02-01 07:37:28 +0000

    converted CoreWeb.csproj to a new format; targetting .net 4.0 and .net 4.5

--- CoreWeb/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("CoreWeb")]
-[assembly: AssemblyProduct("CoreWeb")]
-[assembly: Guid("c94dddb4-339e-4994-b906-b6abfbd0a03d")]
-[assembly: AssemblyVersion("2.0.0.*")]
--- NugetPackages/create_nuget_packages.bat
+++ NugetPackages/create_nuget_packages.bat
@@ -1,6 +1,3 @@
-cd ..\CoreDdd.Register.Castle
-..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
-
 cd ..\CoreIntegrationTest.Nhibernate
 ..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
 
@@ -8,7 +5,4 @@ cd ..\CoreNserviceBusTest
 ..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
 
 cd ..\CoreTest
-..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
-
-cd ..\CoreWeb
 ..\..\.nuget\nuget.exe pack Package.nuspec -OutputDirectory ..\
