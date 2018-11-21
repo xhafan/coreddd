c4cc52ff076401d8e981f45b0cc05e44ba91b936
xhafan <xhafan@gmail.com>
2018-11-21 09:12:30 +0100

    added a xml documentation to unit of work

--- src/CoreDdd/UnitOfWorks/IUnitOfWork.cs
+++ src/CoreDdd/UnitOfWorks/IUnitOfWork.cs
@@ -7,14 +7,37 @@ using System.Threading.Tasks;
 
 namespace CoreDdd.UnitOfWorks
 {
+    /// <summary>
+    /// Represents a unit of work which can start a transaction, commit it or roll it back.
+    /// </summary>
     public interface IUnitOfWork : IDisposable
     {
+        /// <summary>
+        /// Begin a transaction with a given isolation level.
+        /// </summary>
+        /// <param name="isolationLevel">An isolation level</param>
         void BeginTransaction(IsolationLevel isolationLevel = IsolationLevel.Unspecified);
+
+        /// <summary>
+        /// Commit a transaction.
+        /// </summary>
         void Commit();
+
+        /// <summary>
+        /// Rollback a transaction.
+        /// </summary>
         void Rollback();
 
 #if !NET40 && !NET45
+        /// <summary>
+        /// Commit a transaction asynchronously.
+        /// </summary>
         Task CommitAsync();
+
+        /// <summary>
+        /// Rollback a transaction asynchronously.
+        /// </summary>
+        /// <returns></returns>
         Task RollbackAsync();
 #endif
     }
--- src/CoreDdd/UnitOfWorks/IUnitOfWorkFactory.cs
+++ src/CoreDdd/UnitOfWorks/IUnitOfWorkFactory.cs
@@ -1,8 +1,22 @@
 ﻿namespace CoreDdd.UnitOfWorks
 {
+    /// <summary>
+    /// A factory to create an unit of work.
+    /// Usually implemented auto-magically by an IoC container.
+    /// </summary>
     public interface IUnitOfWorkFactory
     {
+        /// <summary>
+        /// Creates a new unit of work.
+        /// </summary>
+        /// <returns></returns>
         IUnitOfWork Create();
+
+        /// <summary>
+        /// Releases a unit of work instance previously created by <see cref="Create"/> method.
+        /// This is needed by Castle Windsor IoC container, other IoC containers (e.g. Ninject, ServiceProvider) don't support it.
+        /// </summary>
+        /// <param name="unitOfWork">An unit of work instance</param>
         void Release(IUnitOfWork unitOfWork);
     }
 }
