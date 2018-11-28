8c182983a1cc19eea12b616d247a94b518e3ac46
xhafan <xhafan@gmail.com>
2018-11-28 12:17:11 +0100

    fixing the database schema creation for SQL server where the schema already exists in the database (this issue https://stackoverflow.com/a/6536366/379279)

--- src/CoreDdd.Nhibernate/DatabaseSchemaGenerators/DatabaseSchemaCreator.cs
+++ src/CoreDdd.Nhibernate/DatabaseSchemaGenerators/DatabaseSchemaCreator.cs
@@ -20,10 +20,21 @@ namespace CoreDdd.Nhibernate.DatabaseSchemaGenerators
             )
         {
             var configuration = nhibernateConfigurator.GetConfiguration();
+
+            // We need to drop the schema twice due to this issue: https://stackoverflow.com/a/6536366/379279
+
             new SchemaExport(configuration).Execute(
                 useStdOut: true,
                 execute: true,
-                justDrop: false,
+                justDrop: true, // just dropping the schema - not creating it
+                connection: connection,
+                exportOutput: null
+            );
+
+            new SchemaExport(configuration).Execute(
+                useStdOut: true,
+                execute: true,
+                justDrop: false, // dropping the schema and creating it
                 connection: connection,
                 exportOutput: null
             );
