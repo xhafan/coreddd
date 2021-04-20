c0bb8fa5d7eb9a382f610b0b3ba83378de968092
Filip Kinský <filip@filovo.net>
2021-04-20 08:50:08 +0200

    add convention for DTO persistence to make them load read-only without NHibernate change tracking to disable unwanted DB updates via DTO changes

---
+++ src/CoreDdd.Nhibernate.Tests/Conventions/when_persisting_dto.cs
@@ -0,0 +1,45 @@
+﻿using CoreDdd.Nhibernate.TestHelpers;
+using IntegrationTestsShared;
+using IntegrationTestsShared.TestEntities;
+using NUnit.Framework;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoreDdd.Nhibernate.Tests.Conventions
+{
+    [TestFixture]
+    public class when_persisting_dto: BasePersistenceTest
+    {
+        [Test]
+        public void dto_is_persisted()
+        {
+            var dto = new TestDto { Name = "abc" };
+
+            UnitOfWork.Session.Save(dto);
+            UnitOfWork.Clear();
+            dto = UnitOfWork.Session.Load<TestDto>(dto.Id);
+
+            dto.Name.ShouldBe("abc");
+        }
+
+        [Test]
+        public void loaded_dto_is_readonly()
+        {
+            var dto = new TestDto { Name = "abc" };
+
+            UnitOfWork.Session.Save(dto);
+            UnitOfWork.Clear();
+            dto = UnitOfWork.Session.Load<TestDto>(dto.Id);
+
+            dto.Name = "xyz!";
+            UnitOfWork.Session.Flush();
+            UnitOfWork.Clear();
+            dto = UnitOfWork.Session.Load<TestDto>(dto.Id);
+
+            dto.Name.ShouldBe("abc");
+        }
+    }
+}
---
+++ src/IntegrationTestsShared/TestEntities/TestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntegrationTestsShared.TestEntities
+{
+    public class TestDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
